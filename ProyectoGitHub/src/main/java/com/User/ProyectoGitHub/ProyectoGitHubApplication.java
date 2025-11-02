b2604224dadf0911b1c905447187ee6a4a60ecb8
Victor Manuel Silva Ormeno <victorsilvaormeno@gmail.com>
2025-11-02 20:39:57 -0300

    creacion proyecto

---
+++ ProyectoGitHub/src/main/java/com/User/ProyectoGitHub/ProyectoGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.User.ProyectoGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGitHubApplication.class, args);
+	}
+
+}
